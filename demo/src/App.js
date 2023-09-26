6cd0b0cde76920bfea3342270b5e18d650546228
sathya1016 <94534183+sathya1016@users.noreply.github.com>
2023-09-26 23:14:09 +0530

    Update App.js

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,12 +1,14 @@
 import HomePage from "./Components/HomePage";
 import LoginPage from "./Components/LoginPage";
 import RegisterPage from "./Components/RegisterPage";
+import NavBar from "./Components/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+    <NavBar/>
           <Route element={<HomePage/>} path="/"/>
           <Route element={<RegisterPage/>} path="/Register" />
           <Route element={<LoginPage/>} path="/login" />
